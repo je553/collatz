c1b1ce71c7477448e99d740b471c3d63a30e2b3c
Jesse <hello@je553.com>
2017-03-06 00:59:18 -0600

    first draft

---
+++ collatz.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def collatz(n):
+    if n % 2 == 0:
+        n = n // 2
+        return n
+    elif n % 2 == 1:
+        n = 3 * n + 1
+        return n
+
+
+def validate(inp):
+    try:
+        int(inp) + 1.0
+        return int(inp)
+    except ValueError:
+        print('That\'s not an integer. Enter a whole number with no decimals.')
+        return 0
+
+while True:
+    print('Enter an integer and see the Collatz sequence at work.')
+    num = validate(input())
+    if num == 'exit':
+        sys.exit()
+    elif num == 0:
+        continue
+    while num != 1:
+        print(collatz(num), end=',')
+        num = collatz(num)
+    print('\n')
